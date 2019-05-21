747185aacb5fb63de7c991f0a8858c07b03eda35
Andrew Van Leuven <a@andrewvanleuven.com>
2019-05-20 20:39:17 -0400

    Migrate old files

---
+++ code/maps.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(tidycensus)
+library(tigris)
+library(openxlsx)
+library(curl)
+library(maptools)
+library(usmap)
+library(sf)
+library(stargazer)
+library(tmap)
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
+###
+setwd("~/Documents/GitHub/R/mainstreet")
+'%!in%' <- function(x,y)!('%in%'(x,y))
+###
+st.msp <- read_csv("data/csv/states.csv")
+communities <- read_csv("data/communities.csv") %>%
+  filter(st == "Iowa") %>% 
+  mutate(GEOID = fips,
+         fips = NULL)
+us <- states(cb = TRUE, resolution = "20m")
+usa <- geo_join(us, st.msp, by = "GEOID")
+usa <- usa[usa$STUSPS %!in% c("HI", "AK", "PR"), ]
+###
+ia.places <- places(cb = TRUE, state = "IA")
+ia.plc <- inner_join(communities,ia.places, by = "GEOID")
+###-------------------------------------Show which states I am covering in my Analysis
+us_msp <- tm_shape(usa, projection = 26916) +
+  tm_fill("msp", title = "Study Area", style = "fixed",
+          breaks = c(0, 1, Inf),
+          palette = c("gray", "navy")) +
+  tm_borders(col = "white") +
+  tm_legend(show=FALSE) +
+  tm_layout(panel.labels = c("Main Street Program Data Coverage"))
+us_msp
+tmap_save(us_msp, "plot/us_msp.png", height=6.5)
+###-------------------------------------Show 
+iowa.st <- states(cb = TRUE, resolution = "20m") %>% filter_state("Iowa")
+  
+ia_msp <- ggplot() +
+  geom_sf(data = iowa.st) + 
+  geom_sf(data = ia.plc)
+ia_msp
+
+ggsave("plot/iowa_msp.png",ia_msp, height=6.5)
+
+
+
+
+
+
+
+
+
