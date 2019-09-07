38ed2268d523ab65d2bf7061a065dba68f8e973b
Andrew Van Leuven <a@andrewvanleuven.com>
2019-09-07 17:26:33 -0400

    addresses that need geocoding

--- code/esri_reshape.R
+++ code/esri_reshape.R
@@ -33,15 +33,35 @@ for (i in 1997:2017) {
   df <- read_csv(csvname) %>% select(id:zipcode,address,match.code,x,y)
   nam <- paste0("esri_",i)
   assign(nam, df)}
-rm(csvname,i,nam)
+rm(csvname,i,nam,df)
 
 data <- rbind(esri_1997,esri_1998,esri_1999,esri_2000,esri_2001,esri_2002,esri_2003,
               esri_2004,esri_2005,esri_2006,esri_2007,esri_2008,esri_2009,esri_2010,
-              esri_2011,esri_2012,esri_2013,esri_2014,esri_2015,esri_2016,esri_2017) %>%
-  filter(match.code %in% c("2","4","X")) %>% write_csv("hidden/esri/iowa.csv")
+              esri_2011,esri_2012,esri_2013,esri_2014,esri_2015,esri_2016,esri_2017) 
 
-unique <- data %>% select(id,address,match.code) %>% arrange(address) %>% 
-  distinct() %>% #write_csv("hidden/esri/geocode_iowa.csv")
-  mutate(po_box = ifelse(grepl("PO BOX", address), 1, 0))
+rm(esri_1997,esri_1998,esri_1999,esri_2000,esri_2001,esri_2002,esri_2003,
+   esri_2004,esri_2005,esri_2006,esri_2007,esri_2008,esri_2009,esri_2010,
+   esri_2011,esri_2012,esri_2013,esri_2014,esri_2015,esri_2016,esri_2017)
 
-freqTab(unique,"po_box")
+parceldata <- data %>% filter(!match.code %in% c("2","4","X")) %>% write_csv("hidden/esri/iowa.csv")
+to_geocode <- data %>% filter(match.code %in% c("2","4","X")) %>% write_csv("hidden/esri/ia_2geo.csv")
+
+unique <- data %>% 
+  mutate(po_box = ifelse(grepl("PO BOX", address), 1, 0),
+         box = ifelse(grepl("BOX", address), 1, 0),
+         mi = ifelse(grepl(" MI ", address), 1, 0),
+         mile = ifelse(grepl(" MILE", address), 1, 0),
+         exception = ifelse(address == "2528 MILE RD CHARLES CITY IA 50616", 1, 0),
+         rr = ifelse(grepl("RR ", address), 1, 0),
+         need_match = ifelse(match.code %in% c("2","4","X"), 1, 0),
+         no_address = ifelse(is.na(address.line.1), 1, 0)) %>%
+  select(id,address,match.code,po_box:no_address) %>% 
+  arrange(address) %>% distinct() %>% 
+  filter(need_match == 1, rr == 0, mi == 0,
+         mile == 0 & exception == 0, no_address == 0) %>% 
+  write_csv("data/csv/esri_geocode.csv") # 15.7 percent CANNOT be geocoded
+
+#freqTab(unique,"po_box")
+#freqTab(unique,"rr")
+#freqTab(unique,"need_match")
+#freqTab(unique,"no_address")
