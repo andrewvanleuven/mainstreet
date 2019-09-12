e3ceeb727b2e6eaf8b0242d7dc2de6a42225bc07
Andrew Van Leuven <a@andrewvanleuven.com>
2019-09-12 17:23:31 -0400

    finished tallying establishments into distance buffer zones

--- code/buffer_tally.R
+++ code/buffer_tally.R
@@ -2,20 +2,17 @@ library(tidyverse)
 library(sf)
 library(rleuven)
 library(tigris)
+library(cowplot)
+library(rlang)
 options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
-df <- read_csv("hidden/esri/iowa_geocoded.csv")
-#df_sample <- df %>%
-#  filter(year == 2000) %>% 
-#  sample_n(.,1000) %>% 
-#  select(id_yr:st,x,y) #%>% 
-#  #write_csv("data/csv/sample_xy.csv")
 # Read in Iowa data -------------------------------------------------------
+df <- read_csv("hidden/esri/iowa_geocoded.csv")
 iowa_crs <- 32615
 iowa <- states(cb = T) %>% filter(STATEFP == "19") %>% st_transform(.,crs = iowa_crs)
 iowa_df <- df %>%
-  filter(year == 2000) %>% 
-  sample_n(.,10000) %>% 
+  #filter(year == 2000) %>% 
+  #sample_n(.,10000) %>% 
   select(id_yr:st,x,y) %>% 
   st_as_sf(., coords = c("x","y"), crs = 4326) %>%
   st_transform(.,crs = iowa_crs) %>% 
@@ -25,15 +22,18 @@ iowa_towns <- read_csv("data/csv/msp_data.csv") %>%
   filter(st == "Iowa", rucc > 2, pop2010 < 30000) %>% 
   left_join(.,(places(state = "19", cb = T) %>% mutate(geoid = as.numeric(GEOID)) %>%
                  select(geoid,geometry) %>% st_transform(.,crs = iowa_crs)), by = "geoid") %>% 
-  select(geoid:rucc,geometry) %>% st_as_sf(crs = iowa_crs)
+  select(geoid,name,geometry) %>% st_as_sf(crs = iowa_crs)
 iowa_sf <- st_read("data/shp/google_earth/Iowa.kml") %>% 
   rename_all(tolower) %>% select(-description) %>% 
   mutate(name = str_remove_all(name, ", IA")) %>% st_transform(.,crs = iowa_crs)  
-iowa_downtowns <- st_intersection(iowa_towns,iowa_sf)
+iowa_downtowns <- st_intersection(iowa_towns,(iowa_sf %>% select(-name))) %>% 
+  select(name,geometry) 
+rm(iowa_sf)
 iowa_roads <- map((counties("IA", cb = TRUE, resolution = "20m") %>% pull(COUNTYFP)), 
-                  ~{roads(state = "19", county = .x)}) %>% 
-  rbind_tigris() %>% st_transform(.,crs = iowa_crs)  
+                  ~{roads(state = "19", county = .x)}) %>% rbind_tigris() %>% st_transform(.,crs = iowa_crs)  
+beepr::beep()
 
+# Check that CRS matches up -----------------------------------------------
 iowa$geometry
 iowa_df$geometry
 iowa_towns$geometry
@@ -54,39 +54,89 @@ ggplot() +
   ggsave("plot/cura.png", width = 20, height = 16)
 
 # Start Buffering Around Towns --------------------------------------------
-one <- iowa_towns %>% filter(name == "Webster City")
-one_sf <- st_intersection(one,(st_buffer(iowa_downtowns, 200, joinStyle = "MITRE", endCapStyle = "SQUARE"))) %>% 
-  mutate(buffer = 5) %>% select(geoid:rucc,buffer,geometry)
-ring5 <- st_difference(one,(st_intersection(one,st_buffer(one_sf, 4*400)))) %>% 
-  mutate(buffer = 5) %>% select(geoid:rucc,buffer,geometry)
-ring4 <- st_difference((st_intersection(one,st_buffer(one_sf, 4*400))),(st_intersection(one,st_buffer(one_sf, 3*400)))) %>% 
-  mutate(buffer = 4) %>% select(geoid:rucc,buffer,geometry)
-ring3 <- st_difference((st_intersection(one,st_buffer(one_sf, 3*400))),(st_intersection(one,st_buffer(one_sf, 2*400)))) %>% 
-  mutate(buffer = 3) %>% select(geoid:rucc,buffer,geometry)
-ring2 <- st_difference((st_intersection(one,st_buffer(one_sf, 2*400))),(st_intersection(one,st_buffer(one_sf, 400)))) %>% 
-  mutate(buffer = 2) %>% select(geoid:rucc,buffer,geometry)
-ring1 <- st_difference((st_intersection(one,st_buffer(one_sf, 400))),(st_intersection(one,st_buffer(one_sf, 15)))) %>% 
-  mutate(buffer = 1) %>% select(geoid:rucc,buffer,geometry)
-ring0 <- st_intersection(one,st_buffer(one_sf, 15)) %>% 
-  mutate(buffer = 0) %>% select(geoid:rucc,buffer,geometry)
-ring_bind <- rbind(ring5,ring4,ring3,ring2,ring1,ring0) %>% arrange(geoid)
-ring_dots <- st_intersection(ring_bind,iowa_df)
-ring_roads <- st_intersection(one,iowa_roads)
+random_map <- function(){
+  n_yr <- sample(1997:2017, 1)
+  ntwn <- sample_n(iowa_downtowns,1) %>% pull(name)
+  titl <- sprintf("%s, Iowa in %s", ntwn, n_yr)
+  town <- (iowa_towns %>% select(-geoid)) %>% filter(name == ntwn)
+  cbd0 <- st_buffer((iowa_downtowns %>% filter(name == ntwn)), 200, joinStyle = "MITRE", endCapStyle = "SQUARE")
+  cbd1 <- st_intersection(town,st_buffer(cbd0, 1*400))
+  cbd2 <- st_intersection(town,st_buffer(cbd0, 2*400))
+  cbd3 <- st_intersection(town,st_buffer(cbd0, 3*400))
+  cbd4 <- st_intersection(town,st_buffer(cbd0, 4*400))
+  buf5 <- st_difference(town,cbd4) %>% select(name,geometry) %>% mutate(buffer = 5)
+  buf4 <- st_difference(cbd4,cbd3) %>% select(name,geometry) %>% mutate(buffer = 4)
+  buf3 <- st_difference(cbd3,cbd2) %>% select(name,geometry) %>% mutate(buffer = 3)
+  buf2 <- st_difference(cbd2,cbd1) %>% select(name,geometry) %>% mutate(buffer = 2)
+  buf1 <- st_difference(cbd1,cbd0) %>% select(name,geometry) %>% mutate(buffer = 1)
+  buf0 <- cbd0 %>% select(name,geometry) %>% mutate(buffer = 0)
+  bind <- rbind(buf5,buf4,buf3,buf2,buf1,buf0) %>% select(name,buffer,geometry)
+  dots <- st_intersection(bind,(iowa_df %>% filter(year == 2008) %>% select(-(id:st))))
+  road <- st_intersection(town,iowa_roads)
+  tabl <- dots %>% group_by(buffer) %>% summarise(sum = n()) %>% pull(sum)
+  nwth <- c("within downtown","within 1/4 mile","within 1/2 mile","within 3/4 mile","within 1 mile","beyond 1 mile")
+  labs <- paste(tabl,nwth,sep = " ")
+  clrs <- c('#c6dbef','#9ecae1','#6baed6','#4292c6','#2171b5','#084594')
+  
+  gplt <- ggplot() +
+    geom_sf(data = buf5, fill = clrs[1], color = NA, alpha = 1) +
+    geom_sf(data = buf4, fill = clrs[2], color = NA, alpha = 1) +
+    geom_sf(data = buf3, fill = clrs[3], color = NA, alpha = 1) +
+    geom_sf(data = buf2, fill = clrs[4], color = NA, alpha = 1) +
+    geom_sf(data = buf1, fill = clrs[5], color = NA, alpha = 1) +
+    geom_sf(data = buf0, fill = clrs[6], color = NA, alpha = 1) +
+    geom_sf(data = road, size = .25, color = "black") +
+    geom_point(data = dots, shape = 21, color = "black", fill = "white", size = .75, stroke = .2,
+               aes(x = utm_E,
+                   y = utm_N)) +
+    geom_point(data = dots, shape = 21, color = "black", size = 4, alpha = 0, 
+               aes(x = utm_E,y = utm_N,
+                   fill = factor(buffer))) +
+    geom_sf(data = town, alpha = 0, color = "black", size = 1) +
+    theme_void() +
+    scale_fill_manual(name = "# of Establishments\nin Each Buffer Zone",
+                      values = rev(clrs),
+                      labels = labs) +
+    guides(fill = guide_legend(reverse = F,
+                               override.aes = list(alpha = 1))) + 
+    ggtitle(titl, subtitle = "Concentric Buffers at Equal Distance Intervals from Downtown") +
+    theme(plot.title = element_text(face = "bold", size = 24, hjust = 0.5), 
+          plot.subtitle = element_text(size = 10, hjust = 0.5),
+          text = element_text(family = "IBM Plex Mono")) + 
+    ggsave("plot/buffertest.png", height = 7, width = 10)
+  
+  rm(n_yr,ntwn,titl,road,labs,nwth,tabl,clrs)
+  rm(town,cbd0,cbd1,cbd2,cbd3,cbd4,buf5,buf4,buf3,buf2,buf1,buf0,bind,dots,name,i)
+  gplt
+}
+random_map()
+freqTab(df,"name")
+# Loop for everything -----------------------------------------------------
+for (i in (iowa_downtowns %>% pull(name))) {
+  town <- (iowa_towns %>% select(-geoid)) %>% filter(name == i)
+  cbd0 <- st_buffer((iowa_downtowns %>% filter(name == i)), 200, joinStyle = "MITRE", endCapStyle = "SQUARE")
+  cbd1 <- st_intersection(town,st_buffer(cbd0, 1*400))
+  cbd2 <- st_intersection(town,st_buffer(cbd0, 2*400))
+  cbd3 <- st_intersection(town,st_buffer(cbd0, 3*400))
+  cbd4 <- st_intersection(town,st_buffer(cbd0, 4*400))
+  buf5 <- st_difference(town,cbd4) %>% select(name,geometry) %>% mutate(buffer = 5)
+  buf4 <- st_difference(cbd4,cbd3) %>% select(name,geometry) %>% mutate(buffer = 4)
+  buf3 <- st_difference(cbd3,cbd2) %>% select(name,geometry) %>% mutate(buffer = 3)
+  buf2 <- st_difference(cbd2,cbd1) %>% select(name,geometry) %>% mutate(buffer = 2)
+  buf1 <- st_difference(cbd1,cbd0) %>% select(name,geometry) %>% mutate(buffer = 1)
+  buf0 <- cbd0 %>% select(name,geometry) %>% mutate(buffer = 0)
+  bind <- rbind(buf5,buf4,buf3,buf2,buf1,buf0) %>% select(name,buffer,geometry)
+  dots <- st_intersection(bind,(iowa_df %>% select(-(id:st))))
+  name <- paste0("loop_",tolower(str_replace_all(i, fixed(" "), "_")))
+  assign(name, dots)}
+beepr::beep()
 
-ggplot() +
-  geom_sf(data = ring5, fill = "red", alpha = .4) +
-  geom_sf(data = ring4, fill = "yellow", alpha = .4) +
-  geom_sf(data = ring3, fill = "orange", alpha = .4) +
-  geom_sf(data = ring2, fill = "blue", alpha = .4) +
-  geom_sf(data = ring1, fill = "green", alpha = .4) +
-  geom_sf(data = ring0, fill = "magenta", alpha = .4) +
-  geom_sf(data = ring_roads, color = "black", size = .2) + 
-  geom_point(data = ring_dots, 
-             aes(x = utm_E,
-                 y = utm_N),
-             size = .25) +
-  theme_void() +
-  ggsave("plot/buffertest.png", height = 7, width = 7)
+iowa_bound <- mget(ls(pattern="loop")) %>% bind_rows() %>% arrange(name,buffer) %>% 
+  separate(id_yr, sep = "_", into = c("id","yr")) %>% mutate(id_yr = paste(id,yr,sep = "_")) %>%
+  select(id_yr,buffer,utm_E,utm_N,geometry) %>% write_csv("hidden/esri/buffer_tally.csv")
 
-freqTab(ring_dots,"buffer")
+for (i in towns) {rm(list=paste0("loop_",tolower(str_replace_all(i, fixed(" "), "_"))))}
+rm(town,cbd0,cbd1,cbd2,cbd3,cbd4,buf5,buf4,buf3,buf2,buf1,buf0,bind,dots,name,i)
 
+iowa_buffered <- inner_join((iowa_bound %>% st_drop_geometry()),df, by = "id_yr") %>% 
+  write_csv("hidden/esri/iowa_buffered.csv")
