21aceade482d9919ac4678ed94c323b24220bede
Andrew Van Leuven <a@andrewvanleuven.com>
2019-10-15 11:44:55 -0400

    updated DiD code

--- code/diff_in_diff.R
+++ code/diff_in_diff.R
@@ -3,8 +3,15 @@ library(sf)
 library(rleuven)
 library(tigris)
 library(plm)
+library(did)
 library(panelr)})
 options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
 
 df <- read_csv("data/csv/panel.csv")
+
+jobs <- panel_data(df, id = townid, wave = time)
+jobs
+
+model <- wbm(downtown_jobs ~ time + treated | town + as.factor(cal_year) | time*treated, data = jobs)
+model
