fe38476e0546511b993d65c30db3d3abbc8cc109
Andrew Van Leuven <a@andrewvanleuven.com>
2019-10-18 18:38:26 -0400

    finished round one of presentation slides

--- code/maps4slides.R
+++ code/maps4slides.R
@@ -9,17 +9,17 @@ options(tigris_class = "sf")
 options(tigris_use_cache = TRUE)
 
 us <- states(cb = T) %>% 
-  mutate(msp = ifelse(STUSPS %in% c("IA","OH","WI"), 1, 0)) %>% 
+  mutate(msp = ifelse(STUSPS %in% c("IA","OH","WI","MI"), 1, 0)) %>% 
   filter(GEOID < 60, !STUSPS %in% c("AK","HI"))
 
 ggplot() +
   geom_sf(data = us,
           aes(fill = factor(msp)),
           color = "black") +
-  scale_fill_manual(values = c("gray","#BB0000")) +
+  scale_fill_manual(values = c("#B0B7BC","#BB0000")) +
   #ggtitle("Main Street Program Data Coverage") +
   coord_sf(crs = st_crs(26916),
            ndiscr = 0) +
   guides(fill=FALSE) +
   theme_void() +
-  ggsave("plot/us_msp_slides.png", height=6.5)
+  ggsave("plot/slides/us_msp.png", height=6.5)
