83b9d375fceb333b5e98ae287f1f1cd08621ec62
Andrew J. Van Leuven <33671616+andrewvanleuven@users.noreply.github.com>
2020-05-29 00:50:23 -0400

    converted price to Real USD; added neighborhood controls

--- code/analysis/real_estate/hedonic.R
+++ code/analysis/real_estate/hedonic.R
@@ -1,16 +1,25 @@
 library(tidyverse)
 library(rleuven)
 library(lubridate)
+library(fredr)
 library(stargazer)
 
 # Read in data ------------------------------------------------------------
-msp <- read_csv("data/csv/universe/oh_universe.csv") %>% filter(st == "Ohio")
+msp <- read_csv("data/csv/universe/msp_universe.csv") %>% 
+  filter(st == "Ohio") %>% select(-pop_2010) 
 df <- read_csv("hidden/datatree/cleaned/datatree_clean.csv") %>% 
   left_join(msp, by = c("distance_to" = "city_fips")) %>% 
-  select(1:4,name:pop_2010,everything())
+  select(1:4,name:cty_seat,everything())
+neighborhood_df <- read_csv("hidden/datatree/cleaned/datatree_oh06.csv")
+cpi <- fredr(series_id = "CPIAUCSL", # maybe try CPIHOSSL --- Consumer Price Index for All Urban Consumers: Housing in U.S. City Average
+             observation_start = as.Date("2000-01-01"),
+             observation_end = as.Date("2019-12-31")) %>% 
+  mutate(inflator = 258.444/value) %>% 
+  rename(rdate = date) %>% select(rdate,inflator)
 
 # Final data cleaning -----------------------------------------------------
-df_clean <- df %>% 
+df_clean_0 <- df %>% 
+  inner_join(neighborhood_df, by = "property_id") %>% 
   filter(owner_occupied == "Y",                                                       # removes any rentals or vacant properties
          current_sale_document_type %in% c(71,27,67,35,49,68,55,20,36,44,69,61,4,22), # removes foreclosures, death transfers, other rare deeds
          !is.na(current_sale_contract_date),                                          # removes any missing values for sale date
@@ -41,6 +50,7 @@ df_clean <- df %>%
          city_name = name,
          address = situs_full_street_address,
          city = situs_city,
+         deck = deck_ind,
          distance = dist_ft,
          state = situs_state,
          zip = situs_zip5,
@@ -50,13 +60,74 @@ df_clean <- df %>%
          sqft = building_area,
          lot_size = lot_size_sq_ft)
 
+df_clean <- df_clean_0 %>% 
+  filter(between(sale_date, as.Date("2000-01-01"), as.Date("2019-12-31"))) %>%        # filter date...only transactions since 2000
+  mutate(d2000 = if_else(between(sale_date, as.Date("2000-01-01"), as.Date("2009-12-31")),1,0),
+         d2010 = if_else(between(sale_date, as.Date("2010-01-01"), as.Date("2019-12-31")),1,0),
+         nbhood_pop = ((d2000*pop_2000)+(d2010*pop_2010)),
+         nbhood_age = ((d2000*median_age_2000)+(d2010*median_age_2010)),
+         nbhood_nonwhite = ((d2000*pct_nonwhite_2000)+(d2010*pct_nonwhite_2010)),
+         nbhood_renting = ((d2000*pct_renting_2000)+(d2010*pct_renting_2010)),
+         nbhood_bachelors = ((d2000*pct_bachelors_2000)+(d2010*pct_bachelors_2010)),
+         nbhood_unempl = ((d2000*civ_unempl_2000)+(d2010*civ_unempl_2010)),
+         nbhood_income = ((d2000*median_income_2000)+(d2010*median_income_2010)),
+         msp_yr = (ymd(sprintf("%d-01-01",msp_yr))),
+         msp_at_sale = if_else(sale_date >= msp_yr,1,0),
+         rdate = round_date(sale_date, "month"),
+         msp_at_sale = replace_na(msp_at_sale, 0),
+         age = year(sale_date)-year_built,
+         construction_yn = if_else(recent_construction == year_built,0,1),
+         construction_before_sale = if_else(recent_construction < year(sale_date),1,1),
+         yrs_since_construction = (year(sale_date)-recent_construction)*construction_yn*construction_before_sale,
+         recent_remodel = if_else(between(yrs_since_construction,1,10),1,0)) %>% 
+  filter(!is.na(city_name)) %>% 
+  left_join(.,cpi, by = "rdate") %>% 
+  mutate(inflator = replace_na(inflator, 1),
+         real_sale_price = round(sale_price*inflator,digits = 0)) %>% #arrange(real_sale_price) 
+  select(property_id,msp_at_sale,msp_accr,msp_affl,distance,in_downtown,              # unique ID, MSP, distance
+         address,city_name,st,cz,rucc,cty_seat,                                       # location
+         sqft,lot_size,age,recent_remodel,bedrooms,total_rooms,                       # structural
+         baths,deck,garage,pool,brick,stories,poor_condition,basement,                # structural
+         sale_date,sale_price,real_sale_price,nbhood_pop:nbhood_income) %>%           # transaction, neighborhood
+  write_csv("hidden/datatree/cleaned/datatree_model.csv")
+
+rm(neighborhood_df,msp,df_clean_0,cpi,df)
+
 # Hedonic price model -----------------------------------------------------
-names(df)
+modeldf <- df_clean
+#modeldf_nonmetro <- df_clean %>% filter(rucc>3) 
 
-modeldf <- df_clean %>% 
-  filter(sale_date > as.Date("2014-12-31"))                                           # filter date...only transactions since 2015
+names(modeldf)
 
-model <- lm(log(sale_price) ~ distance*in_downtown + log(lot_size) + log(sqft) + year_built + bedrooms + baths + stories + basement + pool, 
-            data = modeldf)
+model_0 <- lm(log(sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool,
+              data = modeldf)
 
-stargazer(model, out = "results/real_estate/base_model.html", digits = 1)
+model_1 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool,
+              data = modeldf)
+
+model_2 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool +
+              nbhood_age + nbhood_nonwhite + nbhood_renting + nbhood_bachelors + nbhood_unempl + nbhood_income,
+              data = modeldf)
+
+model_3 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool +
+              #nbhood_age + nbhood_nonwhite + nbhood_renting + nbhood_bachelors + nbhood_unempl + nbhood_income +
+              distance + in_downtown,
+              data = modeldf)
+
+model_3.1 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool +
+              nbhood_age + nbhood_nonwhite + nbhood_renting + nbhood_bachelors + nbhood_unempl + nbhood_income +
+              distance + in_downtown,
+              data = modeldf)
+
+model_4 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool +
+              #nbhood_age + nbhood_nonwhite + nbhood_renting + nbhood_bachelors + nbhood_unempl + nbhood_income +
+              distance + in_downtown + msp_at_sale,
+              data = modeldf)
+
+model_4.1 <- lm(log(real_sale_price) ~ log(lot_size) + log(sqft) + age + bedrooms + baths + stories + basement + pool +
+              nbhood_age + nbhood_nonwhite + nbhood_renting + nbhood_bachelors + nbhood_unempl + nbhood_income +
+              distance + in_downtown + msp_at_sale,
+              data = modeldf)
+
+
+stargazer(model_0,model_1, out = "results/real_estate/hedonic.html", digits = 2)
---
+++ code/data_cleaning/real_estate/oh_clean_06.R
@@ -0,0 +1,155 @@
+suppressMessages({
+  library(tidyverse)
+  library(tidycensus)
+  library(sf)
+  library(fst)
+  library(beepr)
+  library(janitor)
+  library(units)
+  library(rleuven)
+  library(tigris)})
+options(tigris_class = "sf")
+options(tigris_use_cache = TRUE)
+
+# Read in and geocode transactions by block group -------------------------
+df <- read_fst("hidden/datatree/cleaned/datatree_oh03.fst") %>% 
+  select(property_id,situs_longitude,situs_latitude) %>% 
+  distinct() %>% 
+  st_as_sf(., coords = c("situs_longitude","situs_latitude"), crs = 4326, remove = T) %>% 
+  st_transform(.,crs = 2834)
+
+bgs <- block_groups("39", cb = T) %>% st_transform(., crs = 2834)
+geocoded <- df %>% st_intersection(.,bgs)
+
+bg_clean <- geocoded %>% 
+  st_drop_geometry() %>% 
+  rename(bg_fips = GEOID) %>% 
+  select(property_id,bg_fips)  
+
+# Prep for Census & ACS downloads -----------------------------------------
+county_fips <- counties("39",cb = T) %>% 
+  mutate(GEOID = str_sub(GEOID, -3)) %>% 
+  arrange(GEOID) %>% pull(GEOID)
+
+v10 <- load_variables(2010, "sf1", cache = TRUE)
+v10_5 <- load_variables(2014, "acs5", cache = TRUE)
+v_00 <- load_variables(2000, "sf1", cache = TRUE)
+v00 <- load_variables(2000, "sf3", cache = TRUE)
+
+# 2010 Downloads ----------------------------------------------------------
+bg_10 <- get_decennial(year = 2010,
+                       variables = c("P010001","P003001","P003002","P013001","H004001","H004004"), 
+                       state = "39", 
+                       county = county_fips,
+                       geography = "block group")
+bg_10_2 <- get_acs(year = 2014,
+                 variables = c("B19013_001","B23025_003","B23025_005","B17020_002","B17020_001"), 
+                 state = "39", 
+                 county = county_fips,
+                 geography = "block group")
+bg_10_3 <- get_acs(year = 2014,
+                 variables = c("B15003_001","B15003_022","B15003_023","B15003_024","B15003_025"), 
+                 state = "39", 
+                 county = county_fips,
+                 geography = "block group")
+beep()
+
+bg_2010 <- rbind(bg_10_2,bg_10_3) %>% rename(value = estimate) %>% select(-moe) %>% 
+  rbind(bg_10) %>% 
+  select(-NAME) %>% 
+  pivot_wider(names_from = "variable", values_from = "value") %>% 
+  mutate(bg_fips = GEOID,
+         pop_2010 = P010001,
+         median_age_2010 = P013001,
+         pct_nonwhite_2010 = P003002/P003001,
+         pct_renting_2010 = H004004/H004001,
+         pct_bachelors_2010 = (B15003_022+B15003_023+B15003_024+B15003_025)/B15003_001,
+         pct_poverty_2010 = B17020_002/B17020_001,
+         civ_unempl_2010 = B23025_005/B23025_003,
+         median_income_2010 = B19013_001) %>% 
+  select(bg_fips,pop_2010,median_age_2010,pct_nonwhite_2010,pct_renting_2010,
+         pct_bachelors_2010,pct_poverty_2010,civ_unempl_2010,median_income_2010)
+
+
+# 2000 Downloads ----------------------------------------------------------
+bg_00 <- get_decennial(year = 2000,
+                       variables = c("P010001","P003001","P003003","P013001","H004001","H004003"), 
+                       state = "39", 
+                       county = county_fips,
+                       sumfile = "sf1",
+                       geography = "block group") 
+bg_00_2 <- get_decennial(year = 2000,
+                       variables = c("P037001","P037015","P037016","P037017","P037018",
+                                     "P037032","P037033","P037034","P037035"), 
+                       state = "39", 
+                       county = county_fips,
+                       sumfile = "sf3",
+                       geography = "block group")
+bg_00_3 <- get_decennial(year = 2000,
+                       variables = c("P087001","P087002","P053001",
+                                     "P043005","P043007","P043012","P043014"), 
+                       state = "39", 
+                       county = county_fips,
+                       sumfile = "sf3",
+                       geography = "block group")
+
+bg_geo <- get_decennial(year = 2000,
+                       variables = "P010001",
+                       state = "39", 
+                       county = county_fips,
+                       geography = "block group",
+                       geometry = T) 
+
+bg_xw <- bg_geo %>% 
+  st_transform(.,crs = 2834) %>% 
+  rename(bg_fips = GEOID) %>% 
+  select(bg_fips) %>% 
+  st_intersection(df,.)
+beep()
+
+
+bg_2000 <- rbind(bg_00,bg_00_2,bg_00_3) %>% 
+  select(-NAME) %>% 
+  pivot_wider(names_from = "variable", values_from = "value") %>% 
+  mutate(bg_fips = GEOID,
+         pop_2000 = P010001,
+         median_age_2000 = P013001,
+         pct_nonwhite_2000 = P003003/P003001,
+         pct_renting_2000 = H004003/H004001,
+         pct_bachelors_2000 = (P037015+P037016+P037017+P037018+P037032+P037033+P037034+P037035)/P037001,
+         pct_poverty_2000 = P087002/P087001,
+         civ_unempl_2000 = (P043007+P043014)/(P043005+P043012),
+         median_income_2000 = P053001) %>% 
+  select(bg_fips,pop_2000,median_age_2000,pct_nonwhite_2000,pct_renting_2000,
+         pct_bachelors_2000,pct_poverty_2000,civ_unempl_2000,median_income_2000)
+
+
+
+# Neighborhood Data Checklist ---------------------------------------------
+#     2010           |     2000            |
+#--------------------|---------------------|
+# [x] Population     | [x] Population      |
+# [x] Race           | [x] Race            |
+# [x] Age            | [x] Age             |
+# [ ] Income         | [x] Income          |
+# [ ] Employment     | [x] Employment      |
+# [ ] Education      | [x] Education       |
+# [x] Housing Tenure | [x] Housing Tenure  |
+# [ ] Poverty        | [x] Poverty         |
+
+# Merge 2000 and 2010 with properties -------------------------------------
+df_neighborhood_2000 <- bg_xw %>% 
+  st_drop_geometry() %>% 
+  left_join(bg_2000, by = "bg_fips") %>% 
+  filter(!is.na(pct_bachelors_2000)) %>% 
+  select(-bg_fips,-pct_poverty_2000)
+
+df_neighborhood_2010 <- bg_clean %>% 
+  left_join(bg_2010, by = "bg_fips") %>% 
+  select(-pct_poverty_2010) 
+
+df_neighborhood <- inner_join(df_neighborhood_2000,df_neighborhood_2010, by = "property_id") %>% 
+  select(property_id,bg_fips,everything()) %>% 
+  write_csv("hidden/datatree/cleaned/datatree_oh06.csv")
+names(df_neighborhood)
+
